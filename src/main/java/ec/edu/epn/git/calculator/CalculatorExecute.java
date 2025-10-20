221b5b44a73857e6477241411fc733531e6aac70
JSGGY <alekseinavalforever@gmail.com>
2025-10-19 22:51:02 -0500

    Cambio Calculadora Ejecucion

--- src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -2,7 +2,7 @@ package ec.edu.epn.git.calculator;
 
 public class CalculatorExecute {
     public static void main(String[] args) {
-        System.out.println("== Calculator Execute ==");
+        System.out.println("== Calculator Execute Probando Integracion ==");
 
         Calculadora c = new Calculadora();
         double addition = c.sumar(1, 2);
